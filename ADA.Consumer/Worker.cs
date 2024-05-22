cc9f69ba54f35936c6d164bec591b51fad9ba613
Andre Henrique dos Santos <andrehs@outlook.com.br>
2024-05-21 21:25:16 -0300

    Ajustes no worker.

--- ADA.Consumer/Worker.cs
+++ ADA.Consumer/Worker.cs
@@ -18,53 +18,52 @@ public class Worker(
     private readonly ILogger<Worker> _logger = logger;
     private readonly IAppSettings _appSettings = appSettings;
     private readonly IServiceScopeFactory _serviceScopeFactory = serviceScopeFactory;
+    private IConnection? Connection;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using var connection = Connect();
-        using var channel = connection.CreateModel();
-        channel.ExchangeDeclare(exchange: "ada.transacao", type: ExchangeType.Fanout);
-        channel.QueueDeclare(queue: "fraude",
-                             durable: false,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
-        channel.QueueBind(queue: "fraude",
-                          exchange: "ada.transacao",
-                          routingKey: "transacao");
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += async (model, ea) =>
-        {
-            byte[] body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            var transacaoDTO = JsonSerializer.Deserialize<TransacaoDTO>(message);
-            if (transacaoDTO is not null)
-            {
-                using IServiceScope scope = _serviceScopeFactory.CreateScope();
-                try
-                {
-                    _logger.LogInformation("Avaliando a transação da conta {} efetuada em {}.", transacaoDTO.ContaOrigem, transacaoDTO.DataHora.ToString("dd/MM/yyyy HH':'mm':'ss"));
-                    var consumerService = scope.ServiceProvider.GetRequiredService<IConsumerService>();
-                    Transacao transacao = await consumerService.ProcessarTransacaoAsync(transacaoDTO.MapearParaEntidade());
-                    _logger.LogInformation("Possui indício de fraude: {}.\n", transacao.Fraude ? "sim" : "não");
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e, message);
-                }
-            }
-        };
-        channel.BasicConsume(queue: "fraude",
-                             autoAck: true,
-                             consumer: consumer);
-
-        _logger.LogInformation("Consumer started at: {time}", DateTimeOffset.Now);
-
         while (!stoppingToken.IsCancellationRequested)
         {
-        }
+            Connection ??= Connect();
+            using var channel = Connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "ada.transacao", type: ExchangeType.Fanout);
+            channel.QueueDeclare(queue: "fraude",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            channel.QueueBind(queue: "fraude",
+                              exchange: "ada.transacao",
+                              routingKey: "transacao");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += async (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var transacaoDTO = JsonSerializer.Deserialize<TransacaoDTO>(message);
+                if (transacaoDTO is not null)
+                {
+                    using IServiceScope scope = _serviceScopeFactory.CreateScope();
+                    try
+                    {
+                        _logger.LogInformation("Avaliando a transação da conta {ContaOrigem} efetuada em {DataHora}.", transacaoDTO.ContaOrigem, transacaoDTO.DataHora.ToString("dd/MM/yyyy HH':'mm':'ss"));
+                        var consumerService = scope.ServiceProvider.GetRequiredService<IConsumerService>();
+                        Transacao transacao = await consumerService.ProcessarTransacaoAsync(transacaoDTO.MapearParaEntidade());
+                        _logger.LogInformation("Possui indício de fraude: {SimNao}.\n", transacao.Fraude ? "sim" : "não");
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e, message);
+                    }
+                }
+            };
+            channel.BasicConsume(queue: "fraude",
+                                 autoAck: true,
+                                 consumer: consumer);
 
-        return;
+            _logger.LogInformation("Consumer started at: {Time}", DateTimeOffset.Now);
+            await Task.Delay(5000, stoppingToken);
+        }
     }
 
     private IConnection Connect()
@@ -82,9 +81,9 @@ public class Worker(
                 };
                 connection = factory.CreateConnection();
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                _logger.LogError($"Failed to connect to RabbitMQ: {ex.Message}");
+                _logger.LogError(e, "Failed to connect to RabbitMQ {Message}:", e.Message);
                 _logger.LogError("Retrying in 10 seconds...");
                 Task.Delay(10000).Wait();
             }
